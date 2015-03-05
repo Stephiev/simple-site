43bd5a7b5c3d1b086d4e69c647fe3674da27d9e3
Joshua Allen <joshallen@outlook.com>
2015-03-04 20:19:00 -0800

    Random generator server.

--- app/script.js
+++ app/script.js
@@ -1,8 +1,5 @@
-var express = require("express");
-var app = express();
-var port = process.env.PORT || 3000;
-
-var quote = [
+function randomQuote() {
+  var quote = [
     "Hello. My name is Inigo Montoya. You killed my father. Prepare to die.",
     "You're so wise. You're like a miniature Buddha, covered in hair.",
     "All right meow. Hand over your license and registration.",
@@ -13,26 +10,12 @@ var quote = [
     "I am serious... and don't call me Shirley",
     "We got no food, we got no jobs... our PETS' HEADS ARE FALLING OFF!",
     "I made the duck blue because I've never seen a blue duck before and I wanted to see one."
-];
+  ];
+  return quote[Math.floor(Math.random() * quote.length)];
+}
 
-var jokes = [
-  { setup: "Guy walks into a bar",
-    punchline: "Says ouch"}
-];
-
-app.get("/joke", function(req, res) {
-  res.json(jokes[0]);
-});
-
-app.get("/quote", function(req, res) {
-  var randomQuote = Math.floor(Math.random() * quote.length);
-  res.send(quote[randomQuote]);
-});
-
-app.get("/", function(req, res) {
-  res.send("hello, universe");
-});
-
-app.listen(port, function() {
-  console.log('Server started on port ' + port);
+$(function() {
+  $('button').on('click', function() {
+    $('#result').text(randomQuote);
+  });
 });
